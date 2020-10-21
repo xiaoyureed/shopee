e419950508269a02f66fe5397b9727086bf27829
xiaoyu <775000738@qq.com>
2020-10-21 22:52:36 +0800

    es test

--- shopee-search/src/test/java/io/github/xiaoyureed/shopeesearch/ShopeeSearchApplicationTests.java
+++ shopee-search/src/test/java/io/github/xiaoyureed/shopeesearch/ShopeeSearchApplicationTests.java
@@ -1,9 +1,12 @@
 package io.github.xiaoyureed.shopeesearch;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.xiaoyureed.shopeesearch.config.EsConfig;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.search.SearchRequest;
@@ -11,12 +14,18 @@ import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.aggregations.Aggregation;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class ShopeeSearchApplicationTests {
@@ -32,10 +41,19 @@ class ShopeeSearchApplicationTests {
         IndexRequest usersIndexReq = new IndexRequest("users");
 
         usersIndexReq.id("1").source(objectMapper.writeValueAsString(
-                User.builder().age(11).gender("male").name("hello xy").build()), XContentType.JSON);
+                User.builder().age(11).name("xiii").gender("mix")), XContentType.JSON);
 
         IndexResponse usersIndexResp = client.index(usersIndexReq, EsConfig.COMMON_OPTIONS);
         System.out.println(usersIndexResp);
+
+        // batch opts
+//        BulkResponse bulkResp = client.bulk(new BulkRequest().add(usersIndexReq), EsConfig.COMMON_OPTIONS);
+//        if (bulkResp.hasFailures()) {
+//            // error log
+//            List<String> ids = Arrays.stream(bulkResp.getItems()).map(BulkItemResponse::getId).collect(Collectors.toList());
+//            System.out.println(ids);
+//        }
+
     }
 
     @Test
@@ -43,14 +61,46 @@ class ShopeeSearchApplicationTests {
         SearchRequest searchRequest = new SearchRequest();
 
         searchRequest.indices("users");
-        searchRequest.source(new SearchSourceBuilder().query(QueryBuilders.matchAllQuery()));
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder()
+                .query(
+                        QueryBuilders.matchAllQuery()
+//                        QueryBuilders.matchQuery("name", "user2")
+                )
+//                        .from(1)
+//                        .size(4)
+                .aggregation(AggregationBuilders.terms("age_distribution").size(10).field("age"))
+                .aggregation(AggregationBuilders.avg("age_avg").field("age"));
+        System.out.println(searchSourceBuilder);
+        searchRequest.source(searchSourceBuilder);
 
         SearchResponse searchResp = client.search(searchRequest, EsConfig.COMMON_OPTIONS);
         System.out.println(searchResp);
+
+        List<User> collect = Arrays.stream(searchResp.getHits().getHits())
+                .map(SearchHit::getSourceAsString)
+                .map(json -> {
+                    try {
+                        return objectMapper.readValue(json, User.class);
+                    } catch (JsonProcessingException e) {
+                        e.printStackTrace();
+                        throw new RuntimeException();
+                    }
+                })
+                .collect(Collectors.toList());
+        System.out.println(collect);
+
+//        searchResp.getAggregations().asList().forEach(aggregation -> {
+////            String aggregationName = aggregation.getName();
+////            aggregation.getMetadata()
+//        });
+
+
     }
 
-    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
     @Builder
+    @Data
     static class User {
         private String name;
         private String gender;
