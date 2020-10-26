d470c6988fcafbce5c4cf2be7a4f59ee262f7d46
xiaoyu <775000738@qq.com>
2020-10-27 00:09:56 +0800

    cache

--- shopee-product/src/test/java/io/github/xiaoyureed/shopeeproduct/ShopeeProductApplicationTests.java
+++ shopee-product/src/test/java/io/github/xiaoyureed/shopeeproduct/ShopeeProductApplicationTests.java
@@ -1,12 +1,19 @@
 package io.github.xiaoyureed.shopeeproduct;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.xiaoyureed.shopeeproduct.entity.BrandEntity;
 import io.github.xiaoyureed.shopeeproduct.service.BrandService;
+import lombok.Data;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @SpringBootTest
@@ -32,6 +39,34 @@ class ShopeeProductApplicationTests {
         System.out.println(updated);
 
         brandService.removeById(7L);
+    }
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    void testRedis() throws JsonProcessingException {
+        ValueOperations<String, String> valueOps = stringRedisTemplate.opsForValue();
+
+        DD dd = new DD();
+        dd.setId(1L);
+        dd.setName("dd");
+        dd.setPrice(BigDecimal.valueOf(11.1));
+        valueOps.set("dd", objectMapper.writeValueAsString(dd));
+
+        String ddStr = "{\"id\":1,\"name\":\"dd\",\"price\":11.1}";
+
+        Assertions.assertThat(valueOps.get("dd")).isEqualTo(ddStr);
+    }
+
+    @Data
+    static class DD {
+        private Long id;
+        private String name;
+        private BigDecimal price;
     }
 
 }
